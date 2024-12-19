1147082399694f1eaca516f50b6cb66b07c2d548
V. Siva S/o white god <149230859+itzme-siva@users.noreply.github.com>
2024-12-19 11:47:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+// Scroll sections active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    let top = window.scrollY; // Moved outside the loop for performance
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top > offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
+    });
+
+    // Sticky navbar
+    if (top > 100) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+};
+
+// Remove toggle icon and navbar when clicking nav bar links
+navLinks.forEach(link => {
+    link.onclick = () => {
+        menuIcon.classList.remove('fa-xmark');
+        navbar.classList.remove('active');
+    };
+});
+
+// Scroll Reveal
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .service-container, .projects-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+// Typed.js
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Fullstack Developer', 'Web Developer', 'Application Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
